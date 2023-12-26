f6deff445e3d617fffd4d56dd7c6a4c335412da5
AADITYARAJ SINGH CHOUHAN <aadityarajsingh0903@gmail.com>
2023-12-27 04:08:10 +0530

    reverse

--- c++/practice questions/38.cpp
+++ c++/practice questions/38.cpp
@@ -43,6 +43,23 @@ bool search(int arr[],int size,int key){
     return 0;
 
 }
+void reverse(int arr[],int size){
+   int start =0;
+   int end = size-1;
+
+   while(start<=end){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+   }
+
+  for(int i=0;i<size;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+    }
+    
+
 
 int main (){
     int n;
@@ -72,6 +89,8 @@ int main (){
    }else{
     cout<<"key not found. "<<endl;
    }
+
+   reverse(arr,n);
 
 
     return 0;
