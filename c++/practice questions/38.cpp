fd66605eeb2ce635872faf557170028eab708c18
AADITYARAJ SINGH CHOUHAN <aadityarajsingh0903@gmail.com>
2023-12-27 04:28:19 +0530

    swap alternate

--- c++/practice questions/38.cpp
+++ c++/practice questions/38.cpp
@@ -58,6 +58,24 @@ void reverse(int arr[],int size){
   }
   cout<<endl;
     }
+
+  void swap_alter(int arr[],int size){
+
+    for(int i=0;i<size;i=i+2){
+    if(i+1<size){
+
+    int start=i;
+    int next =i+1; 
+
+    swap(arr[start],arr[next]);
+
+    }
+    }
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+  }  
     
 
 
@@ -91,6 +109,8 @@ int main (){
    }
 
    reverse(arr,n);
+
+   swap_alter(arr,n);
 
 
     return 0;
